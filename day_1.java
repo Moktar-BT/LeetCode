93496d3ae4705ff7dfb2d321010b0a2caa0b4653
Moktar-BT <137128511+Moktar-BT@users.noreply.github.com>
2024-01-18 10:16:41 +0100

    day_1

---
+++ day_1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       
+        HashSet<Integer> mySet = new HashSet<>();
+        for (int i = 0 ; i<nums.length; i++){
+           
+           if (mySet.add(nums[i])==false){
+               return true;
+           }
+           
+        }
+        return false;   
+    }
+}
