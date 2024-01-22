d95819978be1e2ad2dab20abde750564374727d0
Moktar-BT <137128511+Moktar-BT@users.noreply.github.com>
2024-01-22 15:53:04 +0100

    3Sum

--- day11.java
+++ day11.java
@@ -1,11 +1,14 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        Set<Integer> seen = new HashSet<>();
-
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                // Skip duplicates for i
+                continue;
+            }
+
             int pointer_j = i + 1;
             int pointer_k = nums.length - 1;
 
@@ -14,11 +17,16 @@ public class Solution {
 
                 if (sum == 0) {
                     List<Integer> triplet = Arrays.asList(nums[i], nums[pointer_j], nums[pointer_k]);
-                    int hash = nums[i] * 100 + nums[pointer_j] * 10 + nums[pointer_k];
+                    result.add(triplet);
 
-                    if (!seen.contains(hash)) {
-                        result.add(triplet);
-                        seen.add(hash);
+                    // Skip duplicates for pointer_j
+                    while (pointer_j < pointer_k && nums[pointer_j] == nums[pointer_j + 1]) {
+                        pointer_j++;
+                    }
+
+                    // Skip duplicates for pointer_k
+                    while (pointer_j < pointer_k && nums[pointer_k] == nums[pointer_k - 1]) {
+                        pointer_k--;
                     }
 
                     pointer_j++;
@@ -30,7 +38,6 @@ public class Solution {
                 }
             }
         }
-
         return result;
     }
 }
